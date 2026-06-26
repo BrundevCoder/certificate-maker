3fd613ee672520f441d8c80e761a90b08dfc7974
BrundevCoder <brunodevdrums@gmail.com>
2026-06-26 23:22:57 +0100

    add download button

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+const downloadBtn = document.getElementById("dBtn");
 const nameDisplayer = document.getElementById("certiName");
 
 let certificateUser = prompt("Name: ");
@@ -5,3 +6,22 @@ let certificateUser = prompt("Name: ");
 if (certificateUser.trim() != null) {
   nameDisplayer.innerText = certificateUser.trim();
 }
+
+function saveCertificate() {
+  const certificate = document.getElementById("certificate");
+
+  html2canvas(certificate, {
+    useCORS: true,
+    allowTaint: true,
+    scale: 2,
+    logging: false,
+    backgroundColor: null
+  }).then(canvas => {
+    let link = document.createElement("a");
+    link.download = `${certificateUser}_certificate`;
+    link.href = canvas.toDataURL();
+    link.click()
+  })
+}
+
+downloadBtn.addEventListener("click", saveCertificate);
